1303eddea80efbd38ef706a947a4a8752397a973
pskotak <eltec@eltecweb.cz>
2025-09-01 21:41:03 +0200

    Inital commit

---
+++ globals.cpp
@@ -0,0 +1,400 @@
+#include <math.h>
+#include "globals.h"
+
+char PrnBuf[PrnBufLen];
+
+bool QuitRequest;
+bool QuitProgram;
+bool DisplayGui = true;
+
+unsigned int SCR_WIDTH = 1600;
+unsigned int SCR_HEIGHT = 900; // 780;
+
+bool Redraw = false;
+bool RedrawMap = false;
+bool MapActive = false;
+
+uint8_t Screen = MainScreen;
+
+// VFH
+float ObstacleDelta = 0.27; // 0.05; // TODO Nacitat z configu
+float MagnitudeThreshold = 0.95; // V jake vzdalenosti [m] od stredu robota bereme prekazky pro binarizaci histogramu // TODO Nacitat z configu
+
+int GoalSector = 0;
+double GoalDiff = 0.0;
+uint32_t GoalLat;
+uint32_t GoalLon;
+double GoalAngleRad;
+double NavAngle;
+double YawAlpha = 0.2;
+
+TGPSWaypoint CurrentWp;
+double WpThreshold = 3.0; // [m]
+
+// OSM map + nav
+float GoalX;
+float GoalY;
+float GoalAngle;
+
+float OldVisYaw;
+
+// Platform
+float Roll = 0.0;
+float Pitch = 0.0;
+float Yaw = 0.0;
+float YawRad = 0.0;
+
+float Velocity = -10.0;
+float AngularVelocity = 0.0;
+float PosX;
+float PosY;
+float PosZ;
+
+float setVelocity = 0.0;
+float setAngularVelocity = 0.0;
+float omegaL = 0.0;
+float omegaR = 0.0;
+
+float GoYaw = 0.0;
+
+//Joystick joy("/dev/input/js0");
+//bool JoyOK = true;
+
+//bool ManualOverride = false;
+
+//bool BigRedSwitch = false;
+//bool BigRedPressed = false;
+//bool BigRedReleased = false;
+
+//bool MissionPressed = false;
+//bool MissionReleased = false;
+
+// Mission Robo Orienteering
+
+//std::vector <TGPSWaypoint> Cones;
+
+// ----------------------------------------------------------------------------
+std::mutex LocMap_mutex;
+
+std::mutex D455_mutex;
+std::mutex T265_mutex;
+
+glm::vec3 BotPos;
+glm::quat BotOrientation;
+
+bool UpdateCams = false;
+cv::Mat DispRGB_image(D455H,D455W,CV_8UC3);
+cv::Mat depth_image(D455H,D455W,CV_16UC1);
+cv::Mat lmap_depth_image(D455H,D455W,CV_16UC1);
+cv::Mat DispSegmented_image;
+cv::Mat SegBin((SegH/2), SegW, CV_8UC1);
+
+bool UpdateGridMap = false;
+
+float ClosestObstacleLeft = 1E7;
+float ClosestObstacleFront = 1E7;
+float ClosestObstacleRight = 1E7;
+float ClosestObstacle = 1E7;
+
+uint16_t YawPixel;
+bool NewYawPixel = false;
+float VisYaw;
+bool SteerMode = false;
+
+// Logging
+//TRecord LogRec;
+
+// ============================================================================
+
+// output = ( (1 - factor) x input^3 ) + ( factor x input )
+// factor = 1 is linear.
+// 0 < factor < 1 is less sensitive in the center, more sensitive at the ends of throw.
+float GetExponential(const float In, const float Ex, const float Lim) {
+    float Q = 0.0;
+    float Cub = In*In*In;
+
+    Q = ((1.0-Ex) * Cub) + (Ex*In);
+    Q = Q * Lim;
+    return Q;
+}
+
+float CalcGPSDistance(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2) {
+ double Lat,Lon,Lat1,Lon1,DeltaLat,DeltaLon,A,C,D;
+
+ Lat = InLat1; Lat = (Lat / 1E7) * DegRad;
+ Lon = InLon1; Lon = (Lon / 1E7) * DegRad;
+ Lat1 = InLat2; Lat1 = (Lat1 / 1E7) * DegRad;
+ Lon1 = InLon2; Lon1 = (Lon1 / 1E7) * DegRad;
+ DeltaLat = Lat1 - Lat; DeltaLon = Lon1 - Lon;
+ A = sin(DeltaLat/2.0) * sin(DeltaLat/2.0) + cos(Lat) * cos(Lat1) * sin(DeltaLon/2.0) * sin(DeltaLon/2.0);
+ C = 2 * atan2(sqrt(A), sqrt(1.0-A));
+ D = (EarthR * C) * 1000.0; // Vzdalenost v metrech
+ return D;
+}
+
+float CalcGPSBearingRad(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2) {
+ double Lat,Lon,Lat1,Lon1,D;
+
+ Lat = InLat1; Lat = (Lat / 1E7) * DegRad;
+ Lon = InLon1; Lon = (Lon / 1E7) * DegRad;
+ Lat1 = InLat2; Lat1 = (Lat1 / 1E7) * DegRad;
+ Lon1 = InLon2; Lon1 = (Lon1 / 1E7) * DegRad;
+ D = atan2(sin(Lon1 - Lon) * cos(Lat1), cos(Lat) * sin(Lat1) - sin(Lat) * cos(Lat1) * cos(Lon1 - Lon));
+ return D;
+}
+
+float CalcGPSBearingDeg(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2) {
+ double Brg;
+ Brg = CalcGPSBearingRad(InLat1,InLon1,InLat2,InLon2);
+ Brg = Brg * RadDeg;
+ Brg = fmod(Brg + 360.0, 360.0);
+ return Brg;
+}
+
+float CalcYawDiff(const float CurrentAngle, const float TargetAngle) {
+ float Diff, AbsDiff;
+
+ Diff = TargetAngle - CurrentAngle;
+ AbsDiff = fabs(Diff);
+ if (AbsDiff <= 180.0) {
+  return AbsDiff == 180 ? AbsDiff : Diff;
+ }
+ else if (TargetAngle > CurrentAngle) {
+  return AbsDiff - 360.0;
+ }
+ else {
+  return 360.0 - AbsDiff;
+ }
+}
+
+/**
+ @brief Konvertuje Lat a Lon ve formatu desetinneho cisla v retezci na format (long * 1E7)
+ @param Coord Souradnice ve formatu desetinneho cisla v retezci
+ @return Souradnice ve formatu (long * 1E7)
+
+ Motivaci je nepouzivat plovouci cisla, aby fungovalo vyhledavani uzlu podle souradnic.
+ Celociselna reprezentace netrpi ztratou presnosti.
+*/
+uint32_t GPStoUL(const char *Coord) {
+ uint32_t L = 0;
+ char C,S[80];
+ int I = 0;
+
+ S[0] = 0; C = *Coord++;
+ while (C == ' ') C = *Coord++; // Leading whitespaces
+ while (C != 0) {
+  if (C != '.') S[I++] = C; S[I] = 0;
+  C = *Coord++;
+  if (I > 8)
+  break;
+ }
+ while (I < 9) {
+  S[I++] = '0';
+ }
+ S[I++] = 0;
+ L = atol(S);
+ return L;
+}
+
+void PolarToCart(TPolarVector* InV) {
+    TPolarVector V;
+
+    V = *InV;
+    V.X = V.Mag * cos(V.Theta);
+    V.Y = V.Mag * sin(V.Theta);
+    *InV = V;
+}
+
+void CartToPolar(TPolarVector* InV) {
+    TPolarVector V;
+
+    V = *InV;
+    V.Mag = sqrt((V.X*V.X)+(V.Y*V.Y));
+    V.Theta = atan2(V.Y,V.X);
+    *InV = V;
+}
+
+float ToRangeDeg(const float a) {
+    float A = a;
+    while (A < 0.0) A = A + 360.0;
+    while (A >= 360.0) A = A - 360.0;
+    return A;
+}
+
+float ToRange2Pi(const float a) {
+    float A = a;
+    while (A < 0.0) A = A + (2*PI);
+    while (A >= (2*PI)) A = A - (2*PI);
+    return A;
+}
+
+int ToZeroMax(const int N, const int Max) {
+    int n = N;
+    if (n < 0) n = 0;
+    if (n > Max) n = Max;
+    return n;
+}
+
+float ToPlusMinusJedna(const float R) {
+    float r;
+    r = R;
+    if (r > 1.0) r = 1.0;
+    if (r < -1.0) r = -1.0;
+    return r;
+}
+
+long LAbs(long L) {
+    if (L >= 0)
+        return L;
+    else
+        return (~L + 1);
+}
+
+void Bresenham(int X1, int Y1, int X2, int Y2, std::vector<TPoint2D> &pts) {
+    TPoint2D point;
+
+    pts.clear();
+    long dx =  abs(X2-X1), sx = X1<X2 ? 1 : -1;
+    long dy = -abs(Y2-Y1), sy = Y1<Y2 ? 1 : -1;
+    long err = dx+dy, e2; // error value e_xy
+    for(;;) {
+        point.x = X1; point.y = Y1;
+        pts.push_back(point);
+        if (X1==X2 && Y1==Y2) break;
+        e2 = 2*err;
+        if (e2 >= dy) { err += dy; X1 += sx; } // e_xy+e_x > 0
+        if (e2 <= dx) { err += dx; Y1 += sy; } // e_xy+e_y < 0
+    }
+}
+
+void BresenhamLim(int X1, int Y1, int X2, int Y2, int Min, int Max, std::vector<TPoint2D> &pts) {
+    TPoint2D point;
+
+    pts.clear();
+    long dx =  abs(X2-X1), sx = X1<X2 ? 1 : -1;
+    long dy = -abs(Y2-Y1), sy = Y1<Y2 ? 1 : -1;
+    long err = dx+dy, e2; // error value e_xy
+    for(;;) {
+        if ((X1 < Min) || (X1 > Max) || (Y1 < Min) || (Y1 > Max)) {
+            break;
+        }
+        else {
+            point.x = X1; point.y = Y1;
+            pts.push_back(point);
+            if (X1==X2 && Y1==Y2) break;
+            e2 = 2*err;
+            if (e2 >= dy) { err += dy; X1 += sx; } // e_xy+e_x > 0
+            if (e2 <= dx) { err += dx; Y1 += sy; } // e_xy+e_y < 0
+        }
+    }
+}
+
+void BresenhamLim2(int X1, int Y1, int X2, int Y2, int Min, int Max, std::vector<TPoint2D> &pts) {
+    TPoint2D point;
+
+    pts.clear();
+    long dx =  abs(X2-X1), sx = X1<X2 ? 1 : -1;
+    long dy = -abs(Y2-Y1), sy = Y1<Y2 ? 1 : -1;
+    long err = dx+dy, e2; // error value e_xy
+    for(;;) {
+        if ((X1 < Min) || (X1 > Max) || (Y1 < Min) || (Y1 > Max)) {
+            break;
+        }
+        else {
+            point.x = X1; point.y = Y1;
+            pts.push_back(point);
+            //if (X1==X2 && Y1==Y2) break;
+            e2 = 2*err;
+            if (e2 >= dy) { err += dy; X1 += sx; } // e_xy+e_x > 0
+            if (e2 <= dx) { err += dx; Y1 += sy; } // e_xy+e_y < 0
+        }
+    }
+}
+
+void BresenhamLim3(int X1, int Y1, int X2, int Y2, int MinX, int MaxX, int MinY, int MaxY, std::vector<TPoint2DInt> &pts) {
+    TPoint2DInt point;
+
+    pts.clear();
+    long dx =  abs(X2-X1), sx = X1<X2 ? 1 : -1;
+    long dy = -abs(Y2-Y1), sy = Y1<Y2 ? 1 : -1;
+    long err = dx+dy, e2; // error value e_xy
+    for(;;) {
+        if ((X1 < MinX) || (X1 > MaxX) || (Y1 < MinY) || (Y1 > MaxY)) {
+            break;
+        }
+        else {
+            point.x = X1; point.y = Y1;
+            pts.push_back(point);
+            if (X1==X2 && Y1==Y2) break;
+            e2 = 2*err;
+            if (e2 >= dy) { err += dy; X1 += sx; } // e_xy+e_x > 0
+            if (e2 <= dx) { err += dx; Y1 += sy; } // e_xy+e_y < 0
+        }
+    }
+}
+
+void Circle(int xm, int ym, int r, std::vector<TPoint2D> &pts) {
+    TPoint2D point;
+
+    int x = -r, y = 0, err = 2-2*r; // II. Quadrant
+    do {
+        point.x = xm-x; point.y = ym+y; pts.push_back(point); // Pixel(xm-x,ym+y,Color); //   I. Quadrant
+        point.x = xm-y; point.y = ym-x; pts.push_back(point); // Pixel(xm-y,ym-x,Color); //  II. Quadrant
+        point.x = xm+x; point.y = ym-y; pts.push_back(point); // Pixel(xm+x,ym-y,Color); // III. Quadrant
+        point.x = xm+y; point.y = ym+x; pts.push_back(point); // Pixel(xm+y,ym+x,Color); //  IV. Quadrant
+        r = err;
+        if (r <= y) err += ++y*2+1;           // e_xy+e_y < 0
+        if (r > x || err > y) err += ++x*2+1; // e_xy+e_x > 0 or no 2nd y-step
+    } while (x < 0);
+}
+
+void CircleLim(int xm, int ym, int r, int MinX, int MaxX, int MinY, int MaxY, std::vector<TPoint2DInt> &pts) {
+    TPoint2DInt pt;
+
+    int x = -r, y = 0, err = 2-2*r; // II. Quadrant
+    do {
+        pt.x = xm-x; pt.y = ym+y; if ((pt.x >= MinX) && (pt.x <= MaxX) && (pt.y >= MinY) && (pt.y <= MaxY)) pts.push_back(pt); // Pixel(xm-x,ym+y,Color); //   I. Quadrant
+        pt.x = xm-y; pt.y = ym-x; if ((pt.x >= MinX) && (pt.x <= MaxX) && (pt.y >= MinY) && (pt.y <= MaxY)) pts.push_back(pt); // Pixel(xm-y,ym-x,Color); //  II. Quadrant
+        pt.x = xm+x; pt.y = ym-y; if ((pt.x >= MinX) && (pt.x <= MaxX) && (pt.y >= MinY) && (pt.y <= MaxY)) pts.push_back(pt); // Pixel(xm+x,ym-y,Color); // III. Quadrant
+        pt.x = xm+y; pt.y = ym+x; if ((pt.x >= MinX) && (pt.x <= MaxX) && (pt.y >= MinY) && (pt.y <= MaxY)) pts.push_back(pt); // Pixel(xm+y,ym+x,Color); //  IV. Quadrant
+        r = err;
+        if (r <= y) err += ++y*2+1;           // e_xy+e_y < 0
+        if (r > x || err > y) err += ++x*2+1; // e_xy+e_x > 0 or no 2nd y-step
+    } while (x < 0);
+}
+
+float Distance2D(const float X1,const float Y1,const float X2,const float Y2) {
+ float d = sqrt( ((X2-X1) * (X2-X1)) + ((Y2-Y1) * (Y2-Y1)) );
+ return d;
+}
+
+rs2_vector T265toSys(const rs2_vector inpos) {
+    rs2_vector out;
+
+    out.x = -inpos.z;
+    out.y = -inpos.x;
+    out.z = inpos.y;
+    return out;
+}
+
+double GPSGoalAngle(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2) {
+ double Lat,Lon,Lat1,Lon1;
+
+ Lat = InLat1; Lat = (Lat / 1E7) * DegRad;
+ Lon = InLon1; Lon = (Lon / 1E7) * DegRad;
+ Lat1 = InLat2; Lat1 = (Lat1 / 1E7) * DegRad;
+ Lon1 = InLon2; Lon1 = (Lon1 / 1E7) * DegRad;
+ double deltaLon = Lon1-Lon;
+ double x = std::cos(Lat1) * std::sin(deltaLon);
+ double y = std::cos(Lat) * std::sin(Lat1) - std::sin(Lat) * std::cos(Lat1) * std::cos(deltaLon);
+ double bearingRad = std::atan2(x,y);
+ return bearingRad;
+}
+
+double AngleDiff(const double BotYaw, const double GoalYaw) {
+    return GoalYaw - BotYaw;
+}
+
+int AngleToSector(const float Angle) {
+    return (Angle / ScanStep) + HistCenter;
+}
---
+++ globals.h
@@ -0,0 +1,359 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <atomic>
+#include <vector>
+#include <cstdint>
+#include <mutex>
+#include <opencv2/opencv.hpp>
+#include <librealsense2/rs.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp> // For glm::quat
+#include <glm/gtx/euler_angles.hpp> // For glm::yawPitchRoll
+
+
+//#include "joystick.h"
+
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+//#define ProgVersion "Saya V0.7.1"
+
+#define PrnBufLen 255
+extern char PrnBuf[PrnBufLen];
+
+extern bool QuitRequest;
+extern bool QuitProgram;
+extern bool DisplayGui;
+
+extern bool Redraw;
+extern bool RedrawMap;
+extern bool MapActive;
+
+#define ODRV_MFGR_ID 0x1209
+#define ODRV_DEV_ID 0x0D32
+
+#define MainScreen 0
+#define SysScreen 10
+#define MapScreen 20
+#define CamScreen 30
+#define RGBScreen 40
+#define SegmentedScreen 45
+#define DepthScreen 50
+
+extern uint8_t Screen;
+
+extern std::mutex D455_mutex;
+extern std::mutex T265_mutex;
+extern bool UpdateCams;
+
+extern std::mutex LocMap_mutex;
+
+// ----------------------------------------------------------------------------
+
+// Union TWB - pristup k wordu po bytech/
+typedef union {
+    uint16_t W; ///< unsigned long
+    unsigned char B[2]; ///< jednotlive byty LSB first
+} TWB;
+
+// Union TLB - pristup k longu po bytech/
+typedef union {
+    uint32_t L; ///< unsigned long
+    unsigned char B[4]; ///< jednotlive byty LSB first
+} TLB;
+
+// Union TLB - pristup k uint64_t po bytech/
+typedef union {
+    uint64_t N; ///< unsigned long
+    uint32_t L[2];
+    uint16_t W[4];
+    unsigned char B[6]; ///< jednotlive byty LSB first
+} TL64B;
+
+// Union TSLB - pristup ke znamenkovemu longu po bytech
+typedef union {
+    int32_t L; ///< long
+    unsigned char B[4]; ///< jednotlive byty LSB first
+} TSLB;
+
+// Union TFB - pristup k floatu po bytech
+typedef union {
+    float F; ///< float
+    unsigned char B[4]; ///< jednotlive byty LSB first
+} TFB;
+
+typedef struct {
+    float x,y,z;
+    float r,g,b;
+} TVertex;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float CosNorm;
+    uint8_t Color;
+} TColoredVertex;
+
+typedef struct {
+    float x,y;
+} TPoint2D;
+
+typedef struct {
+    int x,y;
+} TPoint2DInt;
+
+typedef struct {
+    float x,y,z;
+} TPoint3D;
+
+typedef struct { // 2D bod se vzdalenosti od nejakeho pocatku
+    int x, y;
+    float dist;
+} TPoint2D_D;
+
+typedef struct {
+    uint8_t r,g,b;
+} TRawColor;
+
+extern unsigned int SCR_WIDTH;
+extern unsigned int SCR_HEIGHT;
+
+#define DepthCamScale 0.001 // hloubka je v milimetrech unsigned short (Z16)
+#define D455depth_max 4000.0f // millimeters
+#define DepthCamMaxDistM 4.000 // Maximalni vzdalenost depth v metrech
+
+#define RadDeg 57.295779513082 //57.2957795
+#define DegRad 0.017453292519943 //0.0174532925
+#define PI 3.1415926535897932384626433832795 //3.141592654
+
+#define EarthR_meters 6371000.0 // Earth radius [m]
+#define EarthR 6371.0 // Earth radius [km]
+
+typedef struct {
+    double Mag; // Polar Magnitude
+    double Theta; // Polar Angle - RADIANS
+    double X; // Cartesian X
+    double Y; // Cartesian Y
+} TPolarVector;
+
+typedef struct {
+    uint32_t lat;
+    uint32_t lon;
+} TGPSWaypoint;
+//extern std::vector <TGPSWaypoint> Cones;
+
+// ----------------------------------------------------------------------------
+
+// Intel Cameras
+
+// --- Color Stream Intrinsics ---
+// Width: 848
+// Height: 480
+// Principal Point (px, py): (422.692, 256.959)
+// Focal Length (fx, fy): (420.806, 420.484)
+// Distortion Model: Inverse Brown Conrady
+// Distortion Coefficients (k1, k2, p1, p2, k3): [-0.0550274, 0.0655312, -0.000867809, 4.69469e-05, -0.0211155]
+//
+// --- Color Stream FOV ---
+// Horizontal FOV: 90.43 degrees
+// Vertical FOV:   59.37 degrees
+//
+// --- Depth Stream Intrinsics ---
+// Width: 848
+// Height: 480
+// Principal Point (px, py): (422.76, 238.46)
+// Focal Length (fx, fy): (424.30, 424.30)
+// Distortion Model: Brown Conrady
+// Distortion Coefficients (k1, k2, p1, p2, k3): [0.00, 0.00, 0.00, 0.00, 0.00]
+//
+// --- Depth Stream FOV ---
+// Horizontal FOV: 89.96 degrees
+// Vertical FOV:   58.99 degrees
+
+#define SegW 513
+#define SegH 513
+#define SegChan 3
+extern cv::Mat DispRGB_image;
+extern cv::Mat depth_image;
+extern cv::Mat lmap_depth_image;
+extern cv::Mat DispSegmented_image;
+extern cv::Mat SegBin;
+
+extern uint16_t YawPixel;
+extern bool NewYawPixel;
+extern float VisYaw;
+extern bool SteerMode;
+
+#define D455W 848
+#define D455H 480
+
+#define D455_color_Ppx 422.692
+#define D455_color_Ppy 256.959
+#define D455_color_Fx 420.692
+#define D455_color_Fy 420.37
+#define D455_color_HFOV 90.43
+#define D455_color_VFOV 59.37
+
+#define D455_depth_Ppx 422.759
+#define D455_depth_Ppy 238.457
+#define D455_depth_Fx 424.295
+#define D455_depth_Fy 424.295
+#define D455_depth_HFOV 89.96
+#define D455_depth_VFOV 58.99
+
+#define D455DispScale 0.5f //1.0f //
+#define D455depth_color_max 3000.0f
+
+#define SegmentedMultiDispScale 0.82f
+#define SegmentedDispScale 0.95f
+
+#define Reduced_HFOV (D455_color_HFOV / 848.0 * 513) // Redukce HFOV z 848 na 513 px
+#define PixelAngle (Reduced_HFOV / D455W)
+#define PixelAngleRad ((Reduced_HFOV * DegRad) / D455W)
+
+// Display gui
+#define DispW 1018
+#define DispH 540
+
+#define DispOfs 80
+#define ValOfs 45
+
+#define ExitButtonY (DispH-42)
+#define SideButtonX (DispW-162)
+#define SideButtonW 160
+#define SideButtonH 40
+#define SideButtonStep 42
+
+// VFH
+#define StartAngle -2.35619449f // rad (-135 deg)
+#define EndAngle 2.35619449f // rad (135 deg)
+#define ScanStep 0.003490659f // rad (0.2 deg)
+#define NumSteps 1350
+#define HistCenter (NumSteps/2)
+
+extern float ObstacleDelta; // Jaka odchylka od vektoru oblohy je povazovana za prekazku
+extern float MagnitudeThreshold; // V jake vzdalenosti [m] od stredu robota bereme prekazky pro binarizaci histogramu
+
+extern int GoalSector;
+extern double GoalDiff;
+extern uint32_t GoalLat;
+extern uint32_t GoalLon;
+extern double GoalAngleRad;
+extern double NavAngle;
+extern double YawAlpha;
+
+// OSM map + nav
+extern float GoalX;
+extern float GoalY;
+extern float GoalAngle;
+//extern float GoalAngleRad;
+//extern float NavAngle;
+//extern float YawAlpha;
+extern float OldVisYaw;
+
+#define MapOfsX 35
+#define MapOfsY 35
+
+#define MapEdge 67 // Pixels -> Pixels * MapResolution = MapEdgeLen [meters]
+#define MapCenter 33
+#define MapDiagonal 2.9
+
+// Platform
+extern float Roll;
+extern float Pitch;
+extern float Yaw;
+extern float Velocity;
+extern float AngularVelocity;
+extern float PosX;
+extern float PosY;
+extern float PosZ;
+extern float GoYaw;
+
+extern float YawRad;
+extern TGPSWaypoint CurrentWp;
+extern double WpThreshold; // [m]
+
+extern float setVelocity;
+extern float setAngularVelocity;
+extern float omegaL;
+extern float omegaR;
+
+//extern Joystick joy;
+//extern bool JoyOK;
+
+//extern bool ManualOverride;
+
+//extern bool BigRedSwitch;
+//extern bool BigRedPressed;
+//extern bool BigRedReleased;
+
+//extern bool MissionPressed;
+//extern bool MissionReleased;
+
+//extern float ClosestObstacleLeft;
+//extern float ClosestObstacleFront;
+//extern float ClosestObstacleRight;
+//extern float ClosestObstacle;
+
+extern bool UpdateGridMap;
+extern glm::vec3 BotPos;
+extern glm::quat BotOrientation;
+
+// #define D455W 848
+// #define D455H 480
+
+// Logging
+#define DepthBufSize (D455W*D455H*2)
+#define RGBBufSize (D455W*D455H*3)
+#define LogRGBBufSize ((D455W)*(D455H)*3)
+#define LogDepthBufSize ((D455W)*(D455H)*2)
+typedef struct {
+    uint32_t RecNo; // Record number            
+    float Roll;
+    float Pitch;
+    float Yaw;
+    float Velocity;
+    float AngularVelocity;
+    float PosX;
+    float PosY;
+    float PosZ;
+    float setVelocity;
+    float setAngularVelocity;
+    float omegaL;
+    float omegaR;
+    unsigned char DepthData[DepthBufSize]; // Raw depth image buffer
+    unsigned char RGBData[RGBBufSize]; // Raw RGB image buffer
+} TRecord;
+//extern TRecord LogRec;
+
+// ============================================================================
+extern rs2_vector T265toSys(const rs2_vector inpos);
+extern float GetExponential(const float In, const float Ex, const float Lim);
+
+extern float CalcGPSDistance(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2);
+extern float CalcGPSBearingRad(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2);
+extern float CalcGPSBearingDeg(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2);
+extern float CalcYawDiff(const float CurrentAngle, const float TargetAngle);
+extern uint32_t GPStoUL(const char *Coord);
+
+extern float ToRangeDeg(const float a);
+extern float ToRange2Pi(const float a);
+extern int ToZeroMax(const int N, const int Max);
+extern float ToPlusMinusJedna(const float R);
+extern long LAbs(long L);
+extern float Distance2D(const float X1,const float Y1,const float X2,const float Y2); // Euclidean distance
+extern void PolarToCart(TPolarVector* InV);
+extern void CartToPolar(TPolarVector* InV);
+extern void Bresenham(int X1, int Y1, int X2, int Y2, std::vector<TPoint2D> &pts);
+extern void BresenhamLim(int X1, int Y1, int X2, int Y2, int Min, int Max, std::vector<TPoint2D> &pts);
+extern void BresenhamLim2(int X1, int Y1, int X2, int Y2, int Min, int Max, std::vector<TPoint2D> &pts);
+extern void BresenhamLim3(int X1, int Y1, int X2, int Y2, int MinX, int MaxX, int MinY, int MaxY, std::vector<TPoint2DInt> &pts);
+extern void Circle(int xm, int ym, int r, std::vector<TPoint2D> &pts);
+extern void CircleLim(int xm, int ym, int r, int MinX, int MaxX, int MinY, int MaxY, std::vector<TPoint2DInt> &pts);
+
+extern double GPSGoalAngle(const uint32_t InLat1, const uint32_t InLon1, const uint32_t InLat2, const uint32_t InLon2);
+extern double AngleDiff(const double BotYaw, const double GoalYaw);
+extern int AngleToSector(const float Angle);
+
+#endif
